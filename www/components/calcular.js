0dadc03a67d6ef8919cc5e33e181bbc223e7a6d2
JoaoPedroDosSantos <joaopedro-ken@hotmail.com>
2019-08-02 03:33:36 +0900

    visual/2função

---
+++ www/components/calcular.js
@@ -0,0 +1,19 @@
+$(document).on("click","#btn7",function(){
+  var numero7= 7;
+
+ var re = $("#resultado").val();
+ $("#resultado").val(re+numero7);
+})
+$(document).on("click","#btn8",function(){
+  var numero8= 8;
+
+ var re = $("#resultado").val();
+ $("#resultado").val(re+numero8);
+})
+$(document).on("click","#btn9",function(){
+  var numero9= 9;
+
+ var re = $("#resultado").val();
+ $("#resultado").val(re+numero9);
+})
+
