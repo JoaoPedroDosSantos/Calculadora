8c8e4a65878ce06ff44dc735188e12fb47393534
JoaoPedroDosSantos <joaopedro-ken@hotmail.com>
2019-08-06 23:19:11 +0900

    refazendo algumas coisas

--- www/components/calcular.js
+++ www/components/calcular.js
@@ -1,19 +1,5 @@
-$(document).on("click","#btn7",function(){
-  var numero7= 7;
-
- var re = $("#resultado").val();
- $("#resultado").val(re+numero7);
+$( function(){
+  $("input[name=btn]").click( function(){
+    $("#result").val($("#result").val()+$(this).val());
+  });
 })
-$(document).on("click","#btn8",function(){
-  var numero8= 8;
-
- var re = $("#resultado").val();
- $("#resultado").val(re+numero8);
-})
-$(document).on("click","#btn9",function(){
-  var numero9= 9;
-
- var re = $("#resultado").val();
- $("#resultado").val(re+numero9);
-})
-
