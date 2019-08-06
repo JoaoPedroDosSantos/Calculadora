e840652226e46bb593931b26ad41109f485c48a4
JoaoPedroDosSantos <joaopedro-ken@hotmail.com>
2019-08-07 00:31:58 +0900

    funções de contas basicas

--- www/components/calcular.js
+++ www/components/calcular.js
@@ -7,6 +7,7 @@ var v1, v2, op;
   });
   $("input[name=ce]").click(function(){
     $("#result").val("");
+    $("#op").text("");
   });
   $("input[name=soma]").click(function(){
  if($("#result").val()!=''){
@@ -18,12 +19,85 @@ var v1, v2, op;
    alert("Nehum numero inserido");
  }
   });
+  $("input[name=sub]").click(function(){
+ if($("#result").val()!=''){
+   v1=parseFloat($("#result").val());
+   $("#result").val('');
+   op="sub";
+   $("#op").text($(this).val());
+ }else{
+   alert("Nehum numero inserido");
+ }
+  });
+    $("input[name=mult]").click(function(){
+ if($("#result").val()!=''){
+   v1=parseFloat($("#result").val());
+   $("#result").val('');
+   op="mult";
+   $("#op").text($(this).val());
+ }else{
+   alert("Nehum numero inserido");
+ }
+  });
+   $("input[name=div]").click(function(){
+ if($("#result").val()!=''){
+   v1=parseFloat($("#result").val());
+   $("#result").val('');
+   op="div";
+   $("#op").text($(this).val());
+ }else{
+   alert("Nehum numero inserido");
+ }
+  });
+  $("input[name=por]").click(function(){
+ if($("#result").val()!=''){
+   v1=parseFloat($("#result").val());
+   $("#result").val('');
+   op="por";
+   $("#op").text($(this).val());
+ }else{
+   alert("Nehum numero inserido");
+ }
+  });
+  $("input[name=pot]").click(function(){
+ if($("#result").val()!=''){
+   v1=parseFloat($("#result").val());
+   $("#result").val('');
+   op="pot";
+   $("#op").text($(this).val());
+ }else{
+   alert("Nehum numero inserido");
+ }
+  });
+  $("input[name=raiz]").click(function(){
+ if($("#result").val()!=''){
+   v1=parseFloat($("#result").val());
+   $("#result").val('');
+   op="raiz";
+   $("#op").text($(this).val());
+ }else{
+   alert("Nehum numero inserido");
+ }
+  });
   $("input[name=igual").click(function(){
     if($("#result").val()!=''){
    v2=parseFloat($("#result").val());
    if(op=="soma"){
      $("#result").val(v1+v2);
    }
+   else if(op=="sub"){
+     $("#result").val(v1-v2);
+   }
+   else if(op=="mult"){
+     $("#result").val(v1*v2);
+   }
+   else if(op=="div"){
+     $("#result").val(v1/v2);
+   }
+   else if(op=="por"){
+     $("#result").val(v1*v2/100);
+   }
+  
  }else{
    alert("Nehum numero inserido para efetutuar o calculo");
  }
